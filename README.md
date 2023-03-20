4c1137434cd9d51e38b94100e3a8532610279315
nurgitlab <nurbek.math@gmail.com>
2023-03-20 20:13:02 +0300

    added: database

--- README.md
+++
@@ -1,2 +0,0 @@
-# mern-blog
-Blog on MERN stack
---
+++ server/models/models.js
@@ -0,0 +1,36 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  email: {type: DataTypes.STRING, unique: true},
+  password: {type: DataTypes.STRING},
+  role: {type: DataTypes.STRING, defaultValue: 'USER'}
+})
+
+const Basket = sequelize.define('basket', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Device = sequelize.define('device', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  name: {type: DataTypes.STRING, unique: true, allowNull: false},
+  price: {type: DataTypes.INTEGER, allowNull: false, allowNull: false},
+  rating: {type: DataTypes.INTEGER, defaultValue: 0},
+  img: {type: DataTypes.STRING, allowNull: false},
+})
+
+const Type = sequelize.define('type', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const Brand = sequelize.define('brand', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const Rating = sequelize.define('brand', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
